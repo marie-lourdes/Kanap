ac3cfeee1fb964d57354a0d6fee6e1a090fe37e0
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-09 15:44:45 +0200

    index.html: add DOM alttxt img

--- front/js/productCatalogue.js
+++ front/js/productCatalogue.js
@@ -1,34 +1,42 @@
 let requet= fetch("http://localhost:3000/api/products");
 console.log("requete",requet);
 let request=fetch("http://localhost:3000/api/products");
-let requestProduct= request.then(function(res){
+let requestProducts= request.then(function(res){
     if(res.ok){
         return res.json();  
     };
-    console.log(res);
 }).catch(function(error){
     console.log("error", error);
    
 });
-  
-requestProduct.then(function(catalogue){
-    console.log( "verification des produits kanapé",catalogue) 
-    let productsCatalogue=document.querySelector("#items");
-    for (cata of catalogue){
+function reqProducts(){
+   let req= requestProducts.then(function(catalogue){
+        console.log("verification des produits kanapé",catalogue) 
+        let productsCatalogue=document.querySelector("#items");
+        for(product of catalogue){
+           
+            let linkProduct= document.createElement("a");
+            let articleProducts= document.createElement("article");
+            let imgProduct= document.createElement("img");
+            imgProduct.src=product.imageUrl;
+            imgProduct.alt=product.altTxt;
        
-        let linkProduct= document.createElement("a");
-        let articleProducts= document.createElement("article");
-        let imgProduct= document.createElement("img");
-        imgProduct.src=cata.imageUrl;
+            articleProducts.appendChild(imgProduct);
+            linkProduct.appendChild(articleProducts);
+            productsCatalogue.appendChild(linkProduct);  
+        };
+       
+    });
+    return req;
+};
+ let catalogueProducts= reqProducts();
+ 
+ 
 
-        articleProducts.appendChild(imgProduct);
-        linkProduct.appendChild(articleProducts);
-        productsCatalogue.appendChild(linkProduct);  
-    };   
-   
-});
+console.log("catalogue produit",catalogueProducts);
+
   
-console.log("requetes", requestProduct);
+
 
   
 
