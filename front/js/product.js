d2bf9931c083f3c7a61ac6702ac182d883883769
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-11 15:10:53 +0200

    add create and select elemnt dom of details product

--- front/js/product.js
+++ front/js/product.js
@@ -15,13 +15,23 @@ let productSelected= request.then(function(res){
     console.log("error", error);
    
 });
-
+// recuperation du produit selectionné en objet javascrit 
 productSelected.then(function(productSelect){
+    console.log("produit selectionné",productSelect)// verification du contenu de l objet du produit
+    console.log("titre du produit", productSelect.name)
+    //création des éléments du DOM et affichage dans le DOM de la page produit
     let itemImg= document.querySelector(".item__img");
     let imgProductSelected= document.createElement("img");
     imgProductSelected.src= productSelect.imageUrl;
     imgProductSelected.alt= productSelect.altTxt;
- console.log("produit selectionné",productSelect)
- console.log("titre du produit", productSelect.name)
+    let titleProduct= document.querySelector(".item__content__titlePrice #title");
+    titleProduct.textContent= productSelect.name;
+    let priceProduct= document.querySelector("#title + p > #price");
+    priceProduct.textContent=productSelect.price;
+    let descriptionProduct= document.querySelector(".item__content__description__title + #description");
+    descriptionProduct.textContent= productSelect.description;
+
+   
     itemImg.appendChild(imgProductSelected);
+  
 });
