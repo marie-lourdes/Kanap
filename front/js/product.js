aed2cf0540561d87589aecd3f4561a0d3d06f83b
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-11 14:17:54 +0200

    product.html:create and add to the DOM img product selected

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,11 @@
+//recuperation de l id du produit selectionné sur la page  actuelle du produit
 params= new URLSearchParams(window.location.search);
 idSelected=params.get("id");
 
 console.log("id selectionné id selectionné",idSelected)
+// requête du produit selectionné avec son id
 let request= fetch(`http://localhost:3000/api/products/${idSelected}`);
-console.log("requete",request);
-// Requête GET des données des produits de l'api et transformation des données au format json en objet javascript
+console.log("requete",request);// vérification du code http 
 
 let productSelected= request.then(function(res){
     if(res.ok){
@@ -15,7 +16,12 @@ let productSelected= request.then(function(res){
    
 });
 
-productSelected.then(function(product){
- console.log("produit selectionné",product)
- console.log("titre d produit", product.name)
+productSelected.then(function(productSelect){
+    let itemImg= document.querySelector(".item__img");
+    let imgProductSelected= document.createElement("img");
+    imgProductSelected.src= productSelect.imageUrl;
+    imgProductSelected.alt= productSelect.altTxt;
+ console.log("produit selectionné",productSelect)
+ console.log("titre du produit", productSelect.name)
+    itemImg.appendChild(imgProductSelected);
 });
