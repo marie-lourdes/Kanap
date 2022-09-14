c90dcb93bc3eb76df1acfc456deb3e55a1d5159f
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-14 19:33:23 +0200

    cart.html: update products selected in cart localStorage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,6 @@
+//recuperation du panier depuis le localStorage
 let tabCartStorage= JSON.parse(localStorage.getItem("produits"));
 console.log("produit selected storage",tabCartStorage);
+for( productSelected of tabCartStorage){
+    console.log("produit selctionné panier", productSelected);
+}
