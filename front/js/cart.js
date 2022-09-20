26db9133d756e8fbef93512db37980ff05798dd5
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-20 10:28:40 +0200

    cart.html: add  previousquantity and compare new quantity with old quantitiy no mofified by the val lsitener input

--- front/js/cart.js
+++ front/js/cart.js
@@ -55,12 +55,12 @@ for (let productSelected of tabCartStorage) {
   console.log("total price", totalPrice)
   console.log("total quantité", totalQuantite)
   const totalQuantityElement = document.getElementById("totalQuantity");
-  console.log( "span total quantity",totalQuantity);
+  console.log("span total quantity", totalQuantity);
   const totalPriceElement = document.getElementById("totalPrice");
-  totalQuantityElement.textContent=`${totalQuantite}`;
-  totalPriceElement.textContent=`${totalPrice}`;
- 
-  
+  totalQuantityElement.textContent = `${totalQuantite}`;
+  totalPriceElement.textContent = `${totalPrice}`;
+
+
 
 
   divImgItem.appendChild(imgArticle);
@@ -79,98 +79,115 @@ for (let productSelected of tabCartStorage) {
   sectionBascket.appendChild(articleBascket);
 
 
-  console.log("typeof",typeof inputQuantity.value =="number")
+  console.log("typeof", typeof inputQuantity.value == "number")
 
-// Bouton modifier quantité:Recuperation des quantité modifié avec listener sur les inputs de chaque article des produits du storage
+  // Bouton modifier quantité:Recuperation des quantité modifié avec listener sur les inputs de chaque article des produits du storage
   inputQuantity.addEventListener("change", function (event) {
-    
+
     let val = event.target.value;
-  
+
     console.log("productselected modifié", productSelected.quantite)
 
-   /* selectionner l'ancêtre le plus proche de l'input et qui soit un article avec closest()
-    pour recuperer les datasets des articles  de produits parents de chaque input, correspondant au idproduit et couleur du produit stockée dans la TabCartStorage*/
+    /* selectionner l'ancêtre le plus proche de l'input et qui soit un article avec closest()
+     pour recuperer les datasets des articles  de produits parents de chaque input, correspondant au idproduit et couleur du produit stockée dans la TabCartStorage*/
     let dataArticleProduct = inputQuantity.closest("article");
     let color = dataArticleProduct.getAttribute("data-color");
     let id = dataArticleProduct.getAttribute("data-id");
 
+    let previousQuantite = productSelected.quantite;
+
     console.log("inputQuantity value modifié listener", dataArticleProduct);
+    productSelected.quantite = val;
+    console.log( "productedSelected quantite val",productSelected.quantite)
 
     tabCartStorage.forEach(elem => {
       if (elem.couleur === color && elem.idProduit === id) {
         //recueperation de la quantité modifiée
-        elem.quantite =val;
+        elem.quantite = val;
+        console.log("elem quantité val",elem.quantite)
         // calcul de la quantité modifié avec le prix du produit
-        let calculArticlePrice=productSelected.priceProduct * val;
+        let calculArticlePrice = productSelected.priceProduct * val;
         console.log("price product: ", productSelected.priceProduct);
         // mise à jour de l 'affichage  dans le DOM du prix calculé avec la quantité modifié sans stocker le nouveau prix calculé dans le localstorage
-      
+
         priceItem.textContent = calculArticlePrice + " " + "€";
-        console.log("total quantité modifié",totalQuantite);
+        console.log("total quantité modifié", totalQuantite);
       }
     });
-      
+
     // on reenregistre dans le locastorage le tableau de produits stockés avec les quantité modifié et recupere le tableau modifé de la tabCartStorage
+    localStorage.setItem("produits", JSON.stringify(tabCartStorage));
+    console.log("TAB CART STORAGE", tabCartStorage);
+    tabCartStorage = JSON.parse(localStorage.getItem("produits"));
+
+
+
+    // comparaison  de la quantité du produit et de la quantité modifié du produit par l input  pour incrementer ou desincrementer le total de quantité
+    //function modifTotalQuantite() {
+    console.log(" function total val", val)
+    console.log(" total quantity element ", totalQuantityElement.textContent);
+    console.log(" total quantité function ", totalQuantite);
+    console.log("product selcted total", productSelected.quantite)
+
+    if (productSelected.quantite > previousQuantite) {
+      totalQuantite = totalQuantite + (productSelected.quantite - previousQuantite);
+      totalQuantityElement.textContent = totalQuantite;
+      totalPrice += productSelected.priceProduct * (productSelected.quantite - previousQuantite);
+      totalPriceElement.textContent = totalPrice;
+
       localStorage.setItem("produits", JSON.stringify(tabCartStorage));
-      console.log("TAB CART STORAGE", tabCartStorage);
       tabCartStorage = JSON.parse(localStorage.getItem("produits"));
-    
 
-      // comparaison  de la quantité du produit et de la quantité modifié du produit par l input  pour incrementer ou desincrementer le total de quantité
-   function modifTotalQuantite(){
-    console.log(" function total ",val)
-    console.log(" total quantity element ",totalQuantityElement.textContent);
-    console.log(" total quantité function ",totalQuantite);
-    
-      if(productSelected.quantite <= val){
-        totalQuantite = totalQuantite+1; 
-        
+      console.log(" total quantité function incrementé ", totalQuantite);
 
-       return totalQuantityElement.textContent=totalQuantite;
-    
-    }else if(productSelected.quantite >= val){
-      totalQuantite = totalQuantite-1;
-     
-    
-       return totalQuantityElement.textContent= totalQuantite;
-      
-      
+    } else if (productSelected.quantite < previousQuantite) {
+      totalQuantite = totalQuantite - (previousQuantite - productSelected.quantite);
+      totalQuantityElement.textContent = totalQuantite;
+      totalPrice -= productSelected.priceProduct * (previousQuantite - productSelected.quantite);
+      totalPriceElement.textContent = totalPrice;
+
+      localStorage.setItem("produits", JSON.stringify(tabCartStorage));
+      tabCartStorage = JSON.parse(localStorage.getItem("produits"));
+
+      console.log(" total quantité function descrementé", totalQuantite);
+
+
     };
-    
-  };
 
- modifTotalQuantite();
+    //};
 
-/* localStorage.setItem("produits", JSON.stringify(tabCartStorage));
+    //modifTotalQuantite();
 
-  tabCartStorage = JSON.parse(localStorage.getItem("produits"));
+    localStorage.setItem("produits", JSON.stringify(tabCartStorage));
 
- function modifTotalPrice(){
+    tabCartStorage = JSON.parse(localStorage.getItem("produits"));
 
-  if(productSelected.quantite <= val){
-    console.log("totalprice", totalPrice);
-    totalPrice+=priceArticles;
-    totalPriceElement.textContent= totalPrice;
-   
+    // function modifTotalPrice() {
 
-}else if(productSelected.quantite >= val){
+    //   if (productSelected.quantite <= val) {
+    //     console.log("totalprice", totalPrice);
+    //     totalPrice += priceArticles;
+    //     totalPriceElement.textContent = totalPrice;
 
-  console.log("totalprice", totalPrice);
-    totalPrice-=priceArticles;
-    totalPriceElement.textContent= totalPrice;
-  
-};
-  
-  
 
-  }
- 
+    //   } else if (productSelected.quantite >= val) {
 
-  modifTotalPrice();
-  localStorage.setItem("produits", JSON.stringify(tabCartStorage));
+    //     console.log("totalprice", totalPrice);
+    //     totalPrice -= priceArticles;
+    //     totalPriceElement.textContent = totalPrice;
 
-  tabCartStorage = JSON.parse(localStorage.getItem("produits"));*/
-     
+    //   };
+
+
+
+    // }
+
+
+    //modifTotalPrice();
+    localStorage.setItem("produits", JSON.stringify(tabCartStorage));
+
+    tabCartStorage = JSON.parse(localStorage.getItem("produits"));
+
   });
 
   // bouton supprimer produit
@@ -183,23 +200,23 @@ for (let productSelected of tabCartStorage) {
     console.log("deleteproduct", deleteProduct);
     /* verifier la data color et data-id corresponde au idproduit et couleur du produit de la tabCartStorage avec filter(),
      pour supprimer  supprimer dans la TabCartStorage*/
-     tabCartStorage.forEach(elem => {
+    tabCartStorage.forEach(elem => {
       // supression de l element  par l event click dans le localstorage
-      tabCartStorage=tabCartStorage.filter(function(elem){
-        if(elem.idProduit !== id ) return true;
-        if(elem.couleur!==color) return true;
-      console.log("elem produit",elem);
-     });
+      tabCartStorage = tabCartStorage.filter(function (elem) {
+        if (elem.idProduit !== id) return true;
+        if (elem.couleur !== color) return true;
+        console.log("elem produit", elem);
+      });
 
-     //suppression de l'article du produit  dans le DOM en  generant un tableau avec les elements qui ont un id et une couleur differente de l'element supprimé
-      if (elem.couleur === color && elem.idProduit === id) { 
-         deleteProduct.remove();
-      console.log( "tabcartstorage apres supression element", tabCartStorage);
+      //suppression de l'article du produit  dans le DOM en  generant un tableau avec les elements qui ont un id et une couleur differente de l'element supprimé
+      if (elem.couleur === color && elem.idProduit === id) {
+        deleteProduct.remove();
+        console.log("tabcartstorage apres supression element", tabCartStorage);
       }
-      console.log("evenet",event)
+      console.log("evenet", event)
     });
-         
-  // on reenregistre dans le locastorage le tableau de produits stockés avec les produits non supprimé par le filter  dans la tabCartStorage
+
+    // on reenregistre dans le locastorage le tableau de produits stockés avec les produits non supprimé par le filter  dans la tabCartStorage
     /* on recupere la TabCartStorage avec les elements du DOM generé par la boucle for avec la mise à jour de la tabCartStorage 
    dans lequel la boucle itere sur chaque element et les affiche un a un et sans l element supprimé*/
     localStorage.setItem("produits", JSON.stringify(tabCartStorage));
@@ -212,7 +229,7 @@ for (let productSelected of tabCartStorage) {
 console.log("produit selected storage", tabCartStorage);
 
 
-  
+
 
 
 
