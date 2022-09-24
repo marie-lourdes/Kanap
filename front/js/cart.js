907342406da43943b5fd1c7a6552b366a8e0d798
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-24 22:01:16 +0200

    cart.js => modify comments

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,6 @@
-//................recuperation du panier depuis le localStorage...............
-let tabCartStorage = JSON.parse(localStorage.getItem("produits"));
-console.log("tabcart storage", tabCartStorage)
+//................recupération du panier depuis le localStorage...............
+let tabCartStorage = JSON.parse( localStorage.getItem( "produits" ) );
+console.log( "tabcart storage", tabCartStorage );
 
 /* compteur à zero pour le prix et la quantité  dont les valeurs  s'accumuleront lors de la boucle du panier "tabCartStorage"
  pour donner le total du prix et de la quantité de tous les produits ajoutés au panier et enregistrés dans le localstorage*/
@@ -9,136 +9,132 @@ let totalQuantite = 0;
 
 //.............. récupération et affichage dans le DOM des produits du localStorage..................
 
-for (let productSelected of tabCartStorage) {
-  console.log("produit selctionné panier", productSelected);
-  const sectionBascket = document.getElementById("cart__items");
-  const articleBascket = document.createElement("article");
-  articleBascket.setAttribute("class", "cart__item");
+for( let productSelected of tabCartStorage ){
+  console.log( "produit selctionné panier", productSelected );
+  const sectionBascket = document.getElementById( "cart__items" );
+  const articleBascket = document.createElement( "article" );
+  articleBascket.setAttribute( "class", "cart__item" );
   articleBascket.dataset.id = productSelected.idProduit;
   articleBascket.dataset.color = productSelected.couleur;
 
   // Détails produits
-  const divImgItem = document.createElement("div");
-  divImgItem.setAttribute("class", "cart__item__img");
-  const imgArticle = document.createElement("img");
+  const divImgItem = document.createElement( "div" );
+  divImgItem.setAttribute( "class", "cart__item__img" );
+  const imgArticle = document.createElement( "img" );
   imgArticle.src = productSelected.imgUrl;
   imgArticle.alt = productSelected.altImg;
   const divContentItem = document.createElement("div");
-  divContentItem.setAttribute("class", "cart__item__content");
-  const divDescriptionItem = document.createElement("div");
-  divDescriptionItem.setAttribute("class", "cart__item__content__description");
-  const titleItem = document.createElement("h2");
+  divContentItem.setAttribute( "class", "cart__item__content" );
+  const divDescriptionItem = document.createElement( "div" );
+  divDescriptionItem.setAttribute( "class", "cart__item__content__description" );
+  const titleItem = document.createElement( "h2" );
   titleItem.textContent = productSelected.nameProduct;
-  const colorItem = document.createElement("p");
+  const colorItem = document.createElement( "p" );
   colorItem.textContent = productSelected.couleur;
-  const priceItem = document.createElement("p");
+  const priceItem = document.createElement( "p" );
   let priceArticles = productSelected.priceProduct * productSelected.quantite;
   totalPrice += priceArticles;
-  totalQuantite += parseInt(productSelected.quantite);
+  totalQuantite += parseInt( productSelected.quantite );
   priceItem.textContent = priceArticles + " " + "€";
-  const divContentSettingItem = document.createElement("div");
-  divContentSettingItem.setAttribute("class", "cart__item__content__settings");
-  const divContentQuantityItem = document.createElement("div");
-  divContentQuantityItem.setAttribute("class", "cart__item__content__settings__quantity")
-  const quantityItem = document.createElement("p");
+  const divContentSettingItem = document.createElement( "div" );
+  divContentSettingItem.setAttribute( "class", "cart__item__content__settings" );
+  const divContentQuantityItem = document.createElement( "div" );
+  divContentQuantityItem.setAttribute( "class", "cart__item__content__settings__quantity" )
+  const quantityItem = document.createElement( "p" );
   quantityItem.textContent = "Qté" + " " + ":";
-  const inputQuantity = document.createElement("input");
+  const inputQuantity = document.createElement( "input" );
   inputQuantity.type = "number";
-  inputQuantity.setAttribute("class", "itemQuantity");
-  inputQuantity.setAttribute("name", "itemQuantity");
+  inputQuantity.setAttribute( "class", "itemQuantity" );
+  inputQuantity.setAttribute( "name", "itemQuantity" );
   inputQuantity.min = "1";
   inputQuantity.max = "100";
-  inputQuantity.setAttribute("value", `${productSelected.quantite}`);
-  const divContentDeleteItem = document.createElement("div");
-  divContentDeleteItem.setAttribute("class", "cart__item__content__settings__delete");
-  const deleteItem = document.createElement("p");
-  deleteItem.setAttribute("class", "deleteItem");
+  inputQuantity.setAttribute( "value", `${productSelected.quantite}` );
+  const divContentDeleteItem = document.createElement( "div" );
+  divContentDeleteItem.setAttribute( "class", "cart__item__content__settings__delete" );
+  const deleteItem = document.createElement( "p" );
+  deleteItem.setAttribute( "class", "deleteItem" );
   deleteItem.textContent = "Supprimer";
-  const totalQuantityElement = document.getElementById("totalQuantity");
-  console.log("span total quantity", totalQuantity);
-  const totalPriceElement = document.getElementById("totalPrice");
+  const totalQuantityElement = document.getElementById( "totalQuantity" );
+  console.log( "span total quantity", totalQuantity );
+  const totalPriceElement = document.getElementById( "totalPrice" );
   totalQuantityElement.textContent = `${totalQuantite}`;
   totalPriceElement.textContent = `${totalPrice}`;
-  console.log("total price", totalPrice)
-  console.log("total quantité", totalQuantite)
+  console.log( "total price", totalPrice )
+  console.log( "total quantité", totalQuantite )
 
-  divImgItem.appendChild(imgArticle);
-  articleBascket.appendChild(divImgItem);
-  divDescriptionItem.appendChild(titleItem);
-  divDescriptionItem.appendChild(colorItem);
-  divDescriptionItem.appendChild(priceItem);
-  divContentItem.appendChild(divDescriptionItem);
-  divContentQuantityItem.appendChild(quantityItem);
-  divContentQuantityItem.appendChild(inputQuantity);
-  divContentSettingItem.appendChild(divContentQuantityItem);
-  divContentDeleteItem.appendChild(deleteItem);
-  divContentSettingItem.appendChild(divContentDeleteItem);
-  divContentItem.appendChild(divContentSettingItem);
-  articleBascket.appendChild(divContentItem);
-  sectionBascket.appendChild(articleBascket);
-
- console.log("typeof", typeof inputQuantity.value == "number")
+  divImgItem.appendChild( imgArticle );
+  articleBascket.appendChild( divImgItem );
+  divDescriptionItem.appendChild( titleItem );
+  divDescriptionItem.appendChild( colorItem );
+  divDescriptionItem.appendChild( priceItem );
+  divContentItem.appendChild( divDescriptionItem );
+  divContentQuantityItem.appendChild( quantityItem );
+  divContentQuantityItem.appendChild( inputQuantity );
+  divContentSettingItem.appendChild( divContentQuantityItem );
+  divContentDeleteItem.appendChild( deleteItem );
+  divContentSettingItem.appendChild( divContentDeleteItem );
+  divContentItem.appendChild( divContentSettingItem );
+  articleBascket.appendChild( divContentItem );
+  sectionBascket.appendChild( articleBascket );
   
   //.......................Bouton modifier quantité produit.....................
 
   // Bouton modifier quantité:Recuperation des quantités modifié avec listener sur les inputs de chaque article des produits du storage
-  inputQuantity.addEventListener("change", function (event) {
+  inputQuantity.addEventListener( "change", function ( event ){
     let val = event.target.value;
-    console.log("productselected modifié", productSelected.quantite)
+    console.log( "productselected modifié", productSelected.quantite );
 
     /* selectionner l'ancêtre le plus proche de l'input et qui soit un article avec closest()
      pour recuperer les datasets des articles  de produits parents de chaque input, correspondant au idproduit et couleur du produit stockée dans la TabCartStorage*/
-    let dataArticleProduct = inputQuantity.closest("article");
-    let color = dataArticleProduct.getAttribute("data-color");
-    let id = dataArticleProduct.getAttribute("data-id");
-    console.log("inputQuantity value modifié listener", dataArticleProduct);
+    let dataArticleProduct = inputQuantity.closest( "article" );
+    let color = dataArticleProduct.getAttribute( "data-color" );
+    let id = dataArticleProduct.getAttribute( "data-id" );
+    console.log( "inputQuantity value modifié listener", dataArticleProduct );
 
     // productSelected  sans modification (previousQuantite) et product selected avec la modification de la valeur de l input stockée pour la comparaison pour le total de la quantité et le total du prix 
     let previousQuantite = productSelected.quantite;
     productSelected.quantite = val;
-    console.log( "productedSelected quantite val",productSelected.quantite)
-
-    tabCartStorage.forEach(elem => {
-      if (elem.couleur === color && elem.idProduit === id) {
+    console.log( "productedSelected quantite val", productSelected.quantite );
+    // On reparcourt le tabCartorage pour s'assurer de modifier la quantité correspondant à la data-id et à la data-color de l article du DOM du produit corresponant à l idProduit et à la couleur du produit stocké dans le localStorage
+    // si les id et couleur correspondent au data-id et data-color de l article , on modifie la quantité du produit dans localstorage avec la valeur recuperé par le listener "val"
+    tabCartStorage.forEach( elem => {
+      if( elem.couleur === color && elem.idProduit === id ){
         //recupération de la quantité modifiée
         elem.quantite = val;
-        console.log("elem quantité val",elem.quantite)
+        console.log( "elem quantité val", elem.quantite );
         // calcul de la quantité modifié avec le prix du produit
         let calculArticlePrice = productSelected.priceProduct * val;
-        console.log("price product: ", productSelected.priceProduct);
+        console.log( "price product: ", productSelected.priceProduct );
         // mise à jour de l 'affichage  dans le DOM du prix calculé avec la quantité modifié sans stocker le nouveau prix calculé dans le localstorage
-
         priceItem.textContent = calculArticlePrice + " " + "€";
-        console.log("total quantité modifié", totalQuantite);
+        console.log( "total quantité modifié", totalQuantite );
       }
     });
 
     // création fonction:on reenregistre dans le locastorage le tableau de produits stockés avec les quantité modifiés et recupere le tableau modifé de la tabCartStorage
       
     const updateProductModified= () => {
-      localStorage.setItem("produits", JSON.stringify(tabCartStorage));
-      tabCartStorage = JSON.parse(localStorage.getItem("produits"));
+      localStorage.setItem( "produits", JSON.stringify( tabCartStorage ) );
+      tabCartStorage = JSON.parse( localStorage.getItem( "produits" ) );
     }
     updateProductModified();
-    console.log("tabCartStorage avec quantité modifié", tabCartStorage);
+    console.log( "tabCartStorage avec quantité modifié", tabCartStorage );
 
-    // comparaison  de la quantité du produit et de la quantité modifié du produit par l input  pour incrementer ou desincrementer le total de quantité
+    // comparaison  de la quantité du produit sans modification de la quantité (previousQuantote) et de la quantité modifiée du produit par l input  pour incrementer ou desincrementer le total de quantité
    
-    console.log(" function total val", val)
-    console.log(" total quantity element ", totalQuantityElement.textContent);
-    console.log(" total quantité function ", totalQuantite);
-    console.log("product selcted total", productSelected.quantite)
-
-    if (productSelected.quantite > previousQuantite) {
-      totalQuantite = totalQuantite + (productSelected.quantite - previousQuantite);
+    console.log( " val", val )
+    console.log( "total quantity element avant modification ", totalQuantityElement.textContent );
+    console.log( "total quantité  ", totalQuantite );
+    console.log( "product selected total", productSelected.quantite )
+    // si la quantité modifiée est supérieur à la quantité précédente on incremente la quantité total des articles et on recalcule le prix total avec la difference de quantité entre la variable previousQuantite (quantité non modifié) et la variable productSelected.quantite modifié
+    if( productSelected.quantite > previousQuantite ) {
+      totalQuantite = totalQuantite + ( productSelected.quantite - previousQuantite );
       totalQuantityElement.textContent = totalQuantite;
-      totalPrice += productSelected.priceProduct * (productSelected.quantite - previousQuantite);
+      totalPrice += productSelected.priceProduct * ( productSelected.quantite - previousQuantite );
       totalPriceElement.textContent = totalPrice;
-
       updateProductModified();
-
       console.log(" total quantité function incrementé ", totalQuantite);
-
+    // si la quantité modifiée est inférieure à la quantité précédente on desincremente la quantité total des articles et on recalcule le prix total avec la difference de quantité entre la variable previousQuantite (quantité non modifié) et la variable productSelected.quantite modifié
     } else if (productSelected.quantite < previousQuantite) {
       totalQuantite = totalQuantite - (previousQuantite - productSelected.quantite);
       totalQuantityElement.textContent = totalQuantite;
@@ -189,7 +185,7 @@ for (let productSelected of tabCartStorage) {
 
 console.log("produit selected storage", tabCartStorage);
 
-//....................Formulaire: Récupération dans  un objet contact des données saisies par  l'utilisateur dans le fomulaire................ 
+//....................Formulaire: Récupération dans  un objet contact des données saisies par  l'utilisateur................ 
 
 //création de l objet contact
 const firstName= document.getElementById("firstName");
