298e53c7a95e65838b44fb6935633cb131040421
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-25 16:09:02 +0200

    productCatalogue:  add const

--- front/js/productCatalogue.js
+++ front/js/productCatalogue.js
@@ -15,23 +15,23 @@ let requestProducts = request.then( function( res ){
 requestProducts.then( function( catalogue ){
     console.log( "verification des produits kanapé", catalogue ); 
     // récupération de la section pour rattacher les éléments du DOM
-    let productsCatalogue = document.querySelector( "#items" );
+    const productsCatalogue = document.querySelector( "#items" );
     //récuperation de chaque produit dans l objet de requête avec la boucle for of et affichage dan le Dom de chaque produits
     for( let product of catalogue ){
        //création des élémnts du DOM
-        let linkProduct = document.createElement( "a" );
+        const linkProduct = document.createElement( "a" );
         //création du lien avec id pour chaque produit dans l'url des liens des produits de la page d'accueil
         let idProduct = product._id;
         linkProduct.href = `./product.html?id=${idProduct}`;
         // Création des sous-éléments de chaque lien des produits dans le DOM
-        let articleProducts = document.createElement( "article" );
-        let imgProduct = document.createElement( "img" );
+        const articleProducts = document.createElement( "article" );
+        const imgProduct = document.createElement( "img" );
         imgProduct.src = product.imageUrl;
         imgProduct.alt = product.altTxt;
-        let titleProduct = document.createElement( "h3" );
+        const titleProduct = document.createElement( "h3" );
         titleProduct.textContent = product.name;
         titleProduct.setAttribute( "class", "product-name" );
-        let descriptionProduct = document.createElement( "p" );
+        const descriptionProduct = document.createElement( "p" );
         descriptionProduct.setAttribute( "class", "product-description" );
         descriptionProduct.textContent = product.description;
        
@@ -41,11 +41,14 @@ requestProducts.then( function( catalogue ){
         articleProducts.appendChild( descriptionProduct );
         linkProduct.appendChild( articleProducts );
         productsCatalogue.appendChild( linkProduct );  
-    }    
+    }  
+    console.log("verification  des éléments insérés dans le dom dans la section '#items'",productsCatalogue)  
 })
 .catch( function( error ){
     console.log( "error response", error );  
 });
+
+
     
  
 
